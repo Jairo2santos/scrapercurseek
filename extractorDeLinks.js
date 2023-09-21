56926a8b3ba61cb7276de456c6a0deff57d8719f
Jairo <doblejairo@gmail.com>
2023-09-21 12:22:47 -0300

    version que tiene el extractor de links y el codigo que toma la info de paginas individuales

---
+++ extractorDeLinks.js
@@ -0,0 +1,49 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+
+async function getCursosURLs() {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+
+    const page = await browser.newPage();
+
+    // Desactivar la carga de imágenes para acelerar la navegación
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+        if (req.resourceType() === 'image') {
+            req.abort();
+        } else {
+            req.continue();
+        }
+    });
+
+    let allCourseURLs = [];
+    const totalPages = 37;
+
+    for (let pageIndex = 0; pageIndex <= totalPages; pageIndex++) {
+        const url = `https://sceu.frba.utn.edu.ar/e-learning/listado/Categorias[administracion-de-empresas]${pageIndex ? `?from=${pageIndex}` : ''}`;
+        await page.goto(url);
+
+        const courseURLsOnPage = await page.evaluate(() => {
+            const urls = [];
+            const courseLinkElements = document.querySelectorAll('a.MuiTypography-root.MuiTypography-inherit.MuiLink-root.MuiLink-underlineAlways.css-1cv6iia');
+            
+            courseLinkElements.forEach(linkElement => {
+                urls.push(linkElement.href);
+            });
+        
+            return urls;
+        });
+
+        allCourseURLs = allCourseURLs.concat(courseURLsOnPage);
+    }
+
+    await browser.close();
+
+    // Guardar las URLs en un archivo .txt separadas por comas
+    fs.writeFileSync('courseURLs.txt', allCourseURLs.map(url => `'${url}'`).join(',\n'));
+    console.log('URLs guardadas en courseURLs.txt');
+}
+
+getCursosURLs();
